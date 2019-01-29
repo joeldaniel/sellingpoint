ade8bdb9193af2f99bc97ed42f910138270de710
Anil Kumar Reddy <anilkumarr@maveric-systems.com>
2019-01-30 00:46:37 +0530

    updating docker files

--- compose.sh
+++ compose.sh
@@ -1,3 +1,3 @@
 cd docker
 docker-compose up -d
-docker copy ../target/selling-point-service.war tomcat:/usr/local/tomcat/webapps/
+docker cp ../target/selling-point-service.war tomcat:/usr/local/tomcat/webapps/
